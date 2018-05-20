e7329f5d2b2ba3442d25f24513c661e794b8fbcd
boxdot <d@zerovolt.org>
2018-05-20 21:52:16 +0200

    Add osm flatdata schema and corresponding generated code.
    Also:
    * Implement iterator over osm obf blocks.
    * Open archive for writing.

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,147 @@
 extern crate byteorder;
+extern crate colored;
 extern crate docopt;
+extern crate failure;
+extern crate flate2;
+#[macro_use]
+extern crate flatdata;
 extern crate prost;
 #[macro_use]
 extern crate prost_derive;
 #[macro_use]
 extern crate serde_derive;
-extern crate flate2;
 
 mod args;
+mod osmflat;
 mod osmpbf;
 
 use args::parse_args;
 
 use byteorder::{ByteOrder, NetworkEndian};
+use colored::*;
+use failure::Error;
+use flatdata::{ArchiveBuilder, FileResourceStorage};
 use flate2::read::ZlibDecoder;
 use prost::Message;
 
+use std::cell::RefCell;
 use std::fs::File;
-use std::io::{BufReader, ErrorKind, Read};
+use std::io::{self, BufReader, ErrorKind, Read};
+use std::path::Path;
+use std::rc::Rc;
 
-fn primitrive_group_type(group: &osmpbf::PrimitiveGroup) -> String {
-    if !group.nodes.is_empty() {
-        format!("Nodes {}", group.nodes.len())
-    } else if group.dense.is_some() {
-        String::from("DenseNodes")
-    } else if !group.ways.is_empty() {
-        format!("Ways {}", group.ways.len())
-    } else if !group.relations.is_empty() {
-        format!("Relations {}", group.relations.len())
-    } else if !group.changesets.is_empty() {
-        format!("ChangeSet {}", group.changesets.len())
-    } else {
-        "unknown type".into()
-    }
+#[derive(Debug, Clone)]
+enum OsmPbfBlock {
+    OsmHeader(osmpbf::HeaderBlock),
+    OsmData(osmpbf::PrimitiveBlock),
 }
 
-fn main() {
-    let args = parse_args();
+struct OsmPbfIterator {
+    reader: BufReader<File>,
+    file_buf: Vec<u8>,
+    blob_buf: Vec<u8>,
+    is_open: bool,
+}
 
-    let file = File::open(args.arg_input).unwrap();
-    let mut reader = BufReader::new(file);
+impl OsmPbfIterator {
+    fn new<P: AsRef<Path>>(path: P) -> Result<OsmPbfIterator, Error> {
+        let file = File::open(path)?;
+        Ok(OsmPbfIterator {
+            reader: BufReader::new(file),
+            file_buf: Vec::new(),
+            blob_buf: Vec::new(),
+            is_open: true,
+        })
+    }
 
-    let mut buf = Vec::new();
-    let mut blob_buf = Vec::new();
-
-    loop {
+    fn read_next(&mut self) -> Result<OsmPbfBlock, io::Error> {
         // read size of blob header
-        buf.resize(4, 0);
-        if let Err(e) = reader.read_exact(&mut buf) {
-            if e.kind() == ErrorKind::UnexpectedEof {
-                println!("End of file");
-                return;
-            }
-        }
-        let blob_header_len: i32 = NetworkEndian::read_i32(&buf);
-        println!("blob_header_len = {}", blob_header_len);
+        self.file_buf.resize(4, 0);
+        self.reader.read_exact(&mut self.file_buf)?;
+        let blob_header_len: i32 = NetworkEndian::read_i32(&self.file_buf);
 
         // read blob header
-        buf.resize(blob_header_len as usize, 0);
-        reader.read_exact(&mut buf).unwrap();
-        let blob_header = osmpbf::BlobHeader::decode(&buf).unwrap();
-        println!("{:?}", blob_header);
+        self.file_buf.resize(blob_header_len as usize, 0);
+        self.reader.read_exact(&mut self.file_buf)?;
+        let blob_header = osmpbf::BlobHeader::decode(&self.file_buf)?;
 
         // read blob
-        buf.resize(blob_header.datasize as usize, 0);
-        reader.read_exact(&mut buf).unwrap();
-        let blob = osmpbf::Blob::decode(&buf).unwrap();
-        println!("Blob {{ raw_size = {:?}, ... }}", blob.raw_size,);
+        self.file_buf.resize(blob_header.datasize as usize, 0);
+        self.reader.read_exact(&mut self.file_buf)?;
+        let blob = osmpbf::Blob::decode(&self.file_buf)?;
 
         let blob_data = if blob.raw.is_some() {
             // use raw bytes
             blob.raw.as_ref().unwrap()
         } else if blob.zlib_data.is_some() {
-            // decompress bz data
-            blob_buf.clear();
+            // decompress zlib data
+            self.blob_buf.clear();
             let data: &Vec<u8> = blob.zlib_data.as_ref().unwrap();
             let mut decoder = ZlibDecoder::new(&data[..]);
-            decoder.read_to_end(&mut blob_buf).unwrap();
-            &blob_buf
+            decoder.read_to_end(&mut self.blob_buf)?;
+            &self.blob_buf
         } else {
-            assert!(false, "can only read raw or zlib compressed blob");
-            return;
+            panic!("can only read raw or zlib compressed blob");
         };
         assert_eq!(
             blob_data.len(),
             blob.raw_size.unwrap_or_else(|| blob_data.len() as i32) as usize
         );
 
-        println!("Decompressed size: {}", blob_data.len());
-
-        if blob_header.type_ == "OSMHeader" {
-            let header = osmpbf::HeaderBlock::decode(blob_data).unwrap();
-            println!("{:?}", header);
+        Ok(if blob_header.type_ == "OSMHeader" {
+            OsmPbfBlock::OsmHeader(osmpbf::HeaderBlock::decode(blob_data)?)
         } else if blob_header.type_ == "OSMData" {
-            let block = osmpbf::PrimitiveBlock::decode(blob_data).unwrap();
-            for group in block.primitivegroup.iter() {
-                println!("Group: {:?}", primitrive_group_type(group));
+            OsmPbfBlock::OsmData(osmpbf::PrimitiveBlock::decode(blob_data)?)
+        } else {
+            panic!("unknown blob type");
+        })
+    }
+}
+
+impl Iterator for OsmPbfIterator {
+    type Item = Result<OsmPbfBlock, io::Error>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_open {
+            let next = self.read_next();
+            if let Err(e) = next {
+                if e.kind() == ErrorKind::UnexpectedEof {
+                    self.is_open = false;
+                    None
+                } else {
+                    Some(Err(e))
+                }
+            } else {
+                Some(next)
             }
         } else {
-            assert!(false, "unknown blob type");
-            return;
+            None
         }
+    }
+}
+
+fn run() -> Result<(), Error> {
+    let args = parse_args();
+
+    let pbf_iter = OsmPbfIterator::new(args.arg_input)?;
+    for block in pbf_iter {
+        let block = block?;
+        match block {
+            OsmPbfBlock::OsmHeader(header) => println!("{:?}", header),
+            OsmPbfBlock::OsmData(data) => println!("{}", data.stringtable.s.len()),
+        }
+    }
+
+    let storage = Rc::new(RefCell::new(FileResourceStorage::new(
+        args.arg_output.into(),
+    )));
+    let _builder = osmflat::OsmBuilder::new(storage);
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}: {}", "Error".red(), e);
+        std::process::exit(1);
     }
 }
