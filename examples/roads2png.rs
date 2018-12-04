560de4763b5ff86c178276f0a5aa4e038cfc6a68
Christian Vetter <veaac.fdirct@gmail.com>
2018-12-04 20:42:15 +0100

    Fix review comments and also fix ratio

--- examples/roads2png.rs
+++ examples/roads2png.rs
@@ -15,8 +15,25 @@ use structopt::StructOpt;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::PathBuf;
-
 use std::str;
+
+/// Geographic coordinates represented by (latitude, longitude).
+#[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
+struct GeoCoord {
+    lat: f64,
+    lon: f64,
+}
+
+/// Convert osmflat Node into GeoCoord.
+impl<'a> From<osmflat::RefNode<'a>> for GeoCoord {
+    fn from(node: osmflat::RefNode<'a>) -> Self {
+        const COORD_SCALE: f64 = 1. / osmflat::COORD_SCALE as f64;
+        Self {
+            lat: node.lat() as f64 * COORD_SCALE,
+            lon: node.lon() as f64 * COORD_SCALE,
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Image {
@@ -39,40 +56,44 @@ impl Image {
     }
 }
 
-fn compute_bounds<'a>(mut iter: impl Iterator<Item = RefNode<'a>>) -> ((i64, i64), (i64, i64)) {
-    let first_coord = iter.next().map(|c| (c.lat(), c.lon())).unwrap_or((0, 0));
+fn compute_bounds(mut iter: impl Iterator<Item = GeoCoord>) -> (GeoCoord, GeoCoord) {
+    let first_coord = iter.next().unwrap_or(Default::default());
     iter.fold((first_coord, first_coord), |(min, max), coord| {
         (
-            (min.0.min(coord.lat()), min.1.min(coord.lon())),
-            (max.0.max(coord.lat()), max.1.max(coord.lon())),
+            GeoCoord {
+                lat: min.lat.min(coord.lat),
+                lon: min.lon.min(coord.lon),
+            },
+            GeoCoord {
+                lat: max.lat.max(coord.lat),
+                lon: max.lon.max(coord.lon),
+            },
         )
     })
 }
 
-fn map_transform<'a>(
-    width: u32,
-    height: u32,
-    min: (i64, i64),
-    max: (i64, i64),
-) -> impl FnMut(RefNode<'a>) -> (isize, isize) + Copy {
-    move |coord: RefNode<'a>| {
+fn map_transform(
+    (width, height): (u32, u32),
+    (min, max): (GeoCoord, GeoCoord),
+) -> impl FnMut(GeoCoord) -> (isize, isize) + Copy {
+    move |coord: GeoCoord| {
         (
-            ((coord.lon() - min.1) as f64 / (max.1 - min.1) as f64 * width as f64) as isize,
-            ((max.0 - coord.lat()) as f64 / (max.0 - min.0) as f64 * height as f64) as isize,
+            ((coord.lon - min.lon) * width as f64 / (max.lon - min.lon)) as isize,
+            ((max.lat - coord.lat) * height as f64 / (max.lat - min.lat)) as isize,
         )
     }
 }
 
-fn way_nodes<'a>(
+fn way_coords<'a>(
     archive: &'a osmflat::Osm,
     way: &osmflat::RefWay,
     next_way: &osmflat::RefWay,
-) -> impl Iterator<Item = RefNode<'a>> + 'a {
+) -> impl Iterator<Item = GeoCoord> + 'a {
     let nodes = archive.nodes();
     let nodes_index = archive.nodes_index();
     let begin = way.ref_first_idx() as usize;
     let end = next_way.ref_first_idx() as usize;
-    (begin..end).map(move |i| nodes.at(nodes_index.at(i).value() as usize))
+    (begin..end).map(move |i| nodes.at(nodes_index.at(i).value() as usize).into())
 }
 
 fn substring(strings: &[u8], start: usize) -> &str {
@@ -128,25 +149,28 @@ fn roads<'a>(
 
 fn render(archive: &osmflat::Osm, width: u32) -> Image {
     // compute extent
-    let coords = roads(archive).flat_map(|(way, next_way)| way_nodes(archive, &way, &next_way));
+    let coords = roads(archive).flat_map(|(way, next_way)| way_coords(archive, &way, &next_way));
     let (min, max) = compute_bounds(coords);
 
     // compute ratio and height
-    let ratio = 360. / 180. * (max.0 - min.0) as f64 / (max.1 - min.1) as f64;
+    let ratio =
+        (max.lat - min.lat) / (max.lon - min.lon) / (max.lat / 180. * std::f64::consts::PI).cos();
     let height = (width as f64 * ratio) as u32;
 
     // create world -> raster transformation
-    let t = map_transform(width - 1, height - 1, min, max);
+    let t = map_transform((width - 1, height - 1), (min, max));
 
     // draw
     let mut image = Image::new(width, height);
 
     let line_segments = roads(archive)
-        .flat_map(|(way, next_way)| way_nodes(archive, &way, &next_way).map(t).tuple_windows());
+        .flat_map(|(way, next_way)| way_coords(archive, &way, &next_way).map(t).tuple_windows());
 
-    line_segments
-        .flat_map(|(from, to)| Bresenham::new(from, to))
-        .for_each(|(x, y)| image.set_black(x as u32, y as u32));
+    for (from, to) in line_segments {
+        for (x, y) in Bresenham::new(from, to) {
+            image.set_black(x as u32, y as u32);
+        }
+    }
 
     image
 }
