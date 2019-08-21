8977cdb4ca53befaaa342db1e9235ecf0c8771ab
boxdot <d@zerovolt.org>
2019-08-21 20:55:45 +0200

    Add libosmium examples
    * Read
    * Count
    * Debug

---
+++ examples/count.rs
@@ -0,0 +1,20 @@
+//! Counts the number of nodes, ways, and relations in the input archive.
+//!
+//! LICENSE
+//!
+//! The code in this example file is released into the Public Domain.
+
+use osmflat::{Archive, FileResourceStorage, Osm};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let archive_dir = std::env::args()
+        .nth(1)
+        .ok_or("USAGE: debug <osmflat-archive>")?;
+    let archive = Osm::open(FileResourceStorage::new(archive_dir))?;
+
+    println!("Nodes: {}", archive.nodes().len());
+    println!("Ways: {}", archive.ways().len());
+    println!("Relations: {}", archive.relations().len());
+
+    Ok(())
+}
---
+++ examples/debug.rs
@@ -0,0 +1,241 @@
+//! Dump the contents of the input archive in a debug format.
+//!
+//! Demonstrates
+//! * iteration through all fundamental types.
+//! * accessing of fields and following of references.
+//!
+//! LICENSE
+//!
+//! The code in this example file is released into the Public Domain.
+
+use osmflat::{Archive, FileResourceStorage, Osm, RefRelationMembers};
+
+use std::fmt;
+use std::ops::Range;
+use std::str::Utf8Error;
+
+/// Represents fixed point coordinates stored in OSM
+#[derive(Clone, Copy)]
+struct FixedI64(i64);
+
+impl FixedI64 {
+    fn value(&self) -> f64 {
+        self.0 as f64 / osmflat::COORD_SCALE as f64
+    }
+}
+
+impl fmt::Display for FixedI64 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let value: f64 = self.value();
+        write!(f, "{}", value)
+    }
+}
+
+impl fmt::Debug for FixedI64 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+#[derive(Debug)]
+struct Header<'ar> {
+    bbox: (FixedI64, FixedI64, FixedI64, FixedI64),
+    required_features: Vec<&'ar str>,
+    optional_features: Vec<&'ar str>,
+    writingprogram: &'ar str,
+    source: &'ar str,
+    osmosis_replication_timestamp: i64,
+    osmosis_replication_sequence_number: i64,
+    osmosis_replication_base_url: &'ar str,
+}
+
+impl<'ar> Header<'ar> {
+    fn new(archive: &'ar Osm) -> Result<Self, Utf8Error> {
+        let header = archive.header();
+        let strings = archive.stringtable();
+
+        let required_features: Result<Vec<_>, _> = (header.required_feature_first_idx() as usize..)
+            .take(header.required_features_size() as usize)
+            .map(|idx| strings.substring(idx))
+            .collect();
+        let optional_features: Result<Vec<_>, _> = (header.optional_feature_first_idx() as usize..)
+            .take(header.optional_features_size() as usize)
+            .map(|idx| strings.substring(idx))
+            .collect();
+
+        Ok(Self {
+            bbox: (
+                FixedI64(header.bbox_left()),
+                FixedI64(header.bbox_right()),
+                FixedI64(header.bbox_top()),
+                FixedI64(header.bbox_bottom()),
+            ),
+            required_features: required_features?,
+            optional_features: optional_features?,
+            writingprogram: strings.substring(header.writingprogram_idx() as usize)?,
+            source: strings.substring(header.source_idx() as usize)?,
+            osmosis_replication_timestamp: header.osmosis_replication_timestamp(),
+            osmosis_replication_sequence_number: header.osmosis_replication_sequence_number(),
+            osmosis_replication_base_url: strings
+                .substring(header.osmosis_replication_base_url_idx() as usize)?,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Node<'ar> {
+    id: i64,
+    lat: FixedI64,
+    lon: FixedI64,
+    tags: Vec<Tag<'ar>>,
+}
+
+#[derive(Debug)]
+struct Tag<'ar> {
+    key: &'ar str,
+    value: &'ar str,
+}
+
+impl<'ar> Tag<'ar> {
+    fn new(archive: &'ar Osm, idx: u64) -> Result<Self, Utf8Error> {
+        let strings = archive.stringtable();
+        let tag = archive.tags().at(idx as usize);
+        strings.substring(tag.key_idx() as usize).and_then(|key| {
+            Ok(Tag {
+                key,
+                value: strings.substring(tag.value_idx() as usize)?,
+            })
+        })
+    }
+
+    fn new_slice(
+        archive: &'ar osmflat::Osm,
+        range: Range<u64>,
+    ) -> impl Iterator<Item = Result<Tag<'ar>, Utf8Error>> + 'ar {
+        let tags_index = archive.tags_index();
+        range.map(move |idx| {
+            let idx = tags_index.at(idx as usize).value();
+            Self::new(&archive, idx)
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Way<'ar> {
+    id: i64,
+    tags: Vec<Tag<'ar>>,
+    nodes: Vec<u64>,
+}
+
+#[derive(Debug)]
+struct Relation<'ar> {
+    id: i64,
+    tags: Vec<Tag<'ar>>,
+    members: Vec<Member<'ar>>,
+}
+
+#[derive(Debug)]
+struct Member<'ar> {
+    type_: Type,
+    idx: u64,
+    role: &'ar str,
+}
+
+#[derive(Debug)]
+enum Type {
+    Node,
+    Way,
+    Relation,
+}
+
+impl<'ar> Member<'ar> {
+    fn new_slice(
+        archive: &'ar Osm,
+        relation_idx: usize,
+    ) -> impl Iterator<Item = Result<Member<'ar>, Utf8Error>> {
+        let strings = archive.stringtable();
+        archive
+            .relation_members()
+            .at(relation_idx as usize)
+            .map(move |member| {
+                let res = match member {
+                    RefRelationMembers::NodeMember(m) => Member {
+                        type_: Type::Node,
+                        idx: m.node_idx(),
+                        role: strings.substring(m.role_idx() as usize)?,
+                    },
+                    RefRelationMembers::WayMember(m) => Member {
+                        type_: Type::Way,
+                        idx: m.way_idx(),
+                        role: strings.substring(m.role_idx() as usize)?,
+                    },
+                    RefRelationMembers::RelationMember(m) => Member {
+                        type_: Type::Relation,
+                        idx: m.relation_idx(),
+                        role: strings.substring(m.role_idx() as usize)?,
+                    },
+                };
+                Ok(res)
+            })
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = std::env::args().skip(1).take(2);
+    let archive_dir = args.next().ok_or(
+        "USAGE: debug <osmflat-archive> [TYPES] \
+         TYPES can be any combination of 'n', 'w', 'r' (default: 'nwr').",
+    )?;
+    let types = args.next().unwrap_or("nrw".to_string());
+    let archive = Osm::open(FileResourceStorage::new(archive_dir))?;
+
+    println!("{:#?}", Header::new(&archive)?);
+
+    if types.contains('n') {
+        for node in archive.nodes().slice(..3) {
+            let tags: Result<Vec<_>, _> = Tag::new_slice(&archive, node.tags()).collect();
+            let node = Node {
+                id: node.id(),
+                lat: FixedI64(node.lat()),
+                lon: FixedI64(node.lon()),
+                tags: tags?,
+            };
+
+            println!("{:#?}", node);
+        }
+    }
+
+    let nodes_index = archive.nodes_index();
+
+    if types.contains('w') {
+        for way in archive.ways() {
+            let tags: Result<Vec<_>, _> = Tag::new_slice(&archive, way.tags()).collect();
+            let way = Way {
+                id: way.id(),
+                tags: tags?,
+                nodes: way
+                    .refs()
+                    .map(|idx| nodes_index.at(idx as usize).value())
+                    .collect(),
+            };
+
+            println!("{:#?}", way);
+        }
+    }
+
+    if types.contains('r') {
+        for (relation_idx, relation) in archive.relations().slice(..3).iter().enumerate() {
+            let tags: Result<Vec<_>, _> = Tag::new_slice(&archive, relation.tags()).collect();
+            let members: Result<Vec<_>, _> = Member::new_slice(&archive, relation_idx).collect();
+            let relation = Relation {
+                id: relation.id(),
+                tags: tags?,
+                members: members?,
+            };
+
+            println!("{:#?}", relation);
+        }
+    }
+
+    Ok(())
+}
