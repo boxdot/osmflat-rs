32a5398598a3c855272056143e8b4bcb0969a237
Nick Hallahan <nick@theoutpost.io>
2022-10-25 17:48:37 -0700

    Remove notes.

--- osmflat/recompile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-rm -rf /Users/n/geodata/flatdata/santacruz/*
-cd ../osmflatc
-cargo run --release -- /Users/n/geodata/extracts/santacruz.pbf /Users/n/geodata/flatdata/santacruz
-cd ../osmflat
-cargo run --example sort_hilbert -- /Users/n/geodata/flatdata/santacruz
